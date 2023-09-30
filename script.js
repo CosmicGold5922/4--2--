333a2c60426ed1d0c6be48697ff43b14a2613914
CosmicGold5922 <146548025+CosmicGold5922@users.noreply.github.com>
2023-09-30 18:00:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let arr = ['Баходиров Бехруз','Хакеры','Front-632','Бекзод Каххаров','17:00']
+let boxq = document.querySelector('.box').style = 'display: flex; margin-bottom: 20px; margin: 0 auto; justify-content: center; align-items: center;'
+let name = document.querySelector('.nameq').textContent = arr[0]
+let namea = document.querySelector('.nameq').style = 'color: #0e0d5d; font-style: normal; font-weight: 700; font-size: 24px; line-height: 120%; margin-top: 18px;'
+let xaker = document.querySelector('.box1').textContent = arr[1]
+let box1 = document.querySelector('.box1').style = 'font-style: normal; font-weight: 600; font-size: 16px; line-height: 140%; letter-spacing: .2px; color: #212121; width: 340px; margin: 14px auto;'
+let group = document.querySelector('.box2').textContent = arr[2]
+let box2 = document.querySelector('.box2').style = 'font-style: normal; font-weight: 600; font-size: 16px; line-height: 140%; letter-spacing: .2px; color: #212121; width: 340px; margin: 14px auto;'
+let ustoz = document.querySelector('.box3').textContent = arr[3]
+let box3 = document.querySelector('.box3').style = 'font-style: normal; font-weight: 600; font-size: 16px; line-height: 140%; letter-spacing: .2px; color: #212121; width: 340px; margin: 14px auto;'
+let soat = document.querySelector('.box4').textContent = arr[4]
+let box4 = document.querySelector('.box4').style = 'font-style: normal; font-weight: 600; font-size: 16px; line-height: 140%; letter-spacing: .2px; color: #212121; width: 340px; margin: 14px auto;'
+let img = document.querySelector('img').style = 'border-radius: 50%; margin-top: 20px;'
+let vce = document.querySelector('.vce').style = 'margin: 0 auto; padding-top: 39px; width: 380px; background: #fafafa; box-shadow: 0 20px 100px rgba(15,6,4,.08);border-radius: 16px; text-align: center; position: relative;'
+let body = document.querySelector('body').style = 'background-color: #f1f5f9;'
